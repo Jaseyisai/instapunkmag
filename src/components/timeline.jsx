94a68bc75060469511ab66501a5aeb20835adf9f
Jaseyisai <instapunkmag@gmail.com>
2026-05-25 11:08:56 -0700

    Update timeline.jsx

--- src/components/timeline.jsx
+++ src/components/timeline.jsx
@@ -1,1 +1,21 @@
+import React from 'react';
+import TimelineItem from './TimelineItem'; // We will create this next
+
+const Timeline = ({ items }) => {
+  return (
+    <div className="timeline-container">
+      <h2 className="timeline-header">The History of Sound Waves</h2>
+      <div className="timeline-line">
+        {/* Map through the array of items passed into the component */}
+        {items.map((item, index) => (
+          <TimelineItem key={index} item={item} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Timeline;
+
+// IMPORTANT: We use CSS classes (like 'timeline-container') for styling later.
 
