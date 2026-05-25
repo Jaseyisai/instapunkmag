e156999b2195167332e5935b151508468b8b89e6
Jaseyisai <instapunkmag@gmail.com>
2026-05-25 11:10:48 -0700

    Update TimelineItem.jsx

--- src/components/TimelineItem.jsx
+++ src/components/TimelineItem.jsx
@@ -1,1 +1,17 @@
+import React from 'react';
+
+const TimelineItem = ({ item }) => {
+  return (
+    <div className="timeline-item-wrapper">
+      <div className="timeline-dot"></div>
+      <div className="timeline-content">
+        <h3 className="timeline-title">{item.title}</h3>
+        <p className="timeline-date">({item.year})</p>
+        <p className="timeline-description">{item.description}</p>
+      </div>
+    </div>
+  );
+};
+
+export default TimelineItem;
 
